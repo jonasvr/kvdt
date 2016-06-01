5148113403ab735bf7ad8b49f5388e320b7b7821
root <jonasvanreeth@gmail.com>
2016-06-01 13:01:16 +0200

    cleanup profileController

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -10,14 +10,19 @@ use Auth;
 
 class ProfileController extends Controller
 {
+    protected $devices;
+
+    public function __construct(Devices $devices)
+    {
+        $this->devices = $devices;
+        parent::__construct();
+    }
+
     public function addDevice(Request $request){
         $data = $request->all();
-        // dd($data);
-        $input = new Devices();
-        $input->user_id = Auth::user()->id;
-        $input->device_id = $data['device_id'];
-        $input->save();
+        $data['user_id'] = $this->user_id;
+        $this->devices->create($data);
 
-        return Redirect()->route('devices');
+        return redirect()->route('devices');
     }
 }
