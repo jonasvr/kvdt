e138e2ebbc7e67d14d94f3e5d471c2986c8c258b
root <jonasvanreeth@gmail.com>
2016-06-14 09:15:31 +0200

    layout profile

--- resources/views/profile/device.blade.php
+++ resources/views/profile/device.blade.php
@@ -1,21 +1,51 @@
-<h1>Your devices</h1>
-@if(isset($devices))
-    @foreach($devices as $key => $device)
-        @if(!($device['device_type']=='shower'))
-            <p>{{$device->device_type}} =>  {{$device->name}}</p>
-        @endif
-    @endforeach
-@endif
-{{ Form::open(array('url' => URL::route('addDevice'), 'method' => 'Post','class'=>'')) }}
-<div class="form-group">
-    {{ Form::Label('device_id', 'Device ID')}}
-    {{ Form::text('device_id','',['class' => 'form-control']) }}
-</div>
-<div class="form-group">
-    {{ Form::Label('name', 'Name')}}
-    {{ Form::text('name','',['class' => 'form-control']) }}
-</div>
-{{ Form::submit('Submit!',['class'=>'btn btn-default']) }}
-{{ Form::close() }}
+<div class="panel panel-primary">
+    <div class="panel-heading"><h1>Your devices</h1>
+        {{ Form::open(array('url' => URL::route('addDevice'), 'method' => 'Post','class'=>'')) }}
+        <div class="form-group">
+            {{ Form::Label('device_id', 'Device ID')}}
+            {{ Form::text('device_id','',['class' => 'form-control']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::Label('name', 'Name')}}
+            {{ Form::text('name','',['class' => 'form-control']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::submit('Submit!',['class'=>'btn btn-default']) }}
+        </div>
+        {{ Form::close() }}
 
-<h1>Wekker</h1>   {{--<button class="col-md-3">numbers</button>--}}
+        @if(count($devices) > 0)
+            <div class="panel-footer">
+                <div class="row">
+                @foreach($devices as $key => $device)
+                        @if(!($device['device_type']=='shower'))
+                        <div class="col-lg-3 col-md-6">
+                            <div class="panel panel-yellow">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-offset-2 col-xs-10 text-right">
+                                            <div class="huge text-capitalize">{{$device->device_type}}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">{{$device->name}}</span>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        @endif
+
+
+
+                    {{--@if(!($device['device_type']=='shower'))--}}
+                        {{--<p>{{$device->device_type}} =>  {{$device->name}}</p>--}}
+                    {{--@endif--}}
+                @endforeach
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
--- resources/views/profile/kot.blade.php
+++ resources/views/profile/kot.blade.php
@@ -1,33 +1,42 @@
-@if(Auth::user()->koten_id > 0)
-    <div>
-        <h1>KOT</h1>
+<div class="panel panel-green">
+    <div class="panel-heading">
+        <div>
+            <h1>Your Kot</h1>
+        </div>
+        @if(Auth::user()->koten_id > 0)
+
+            @if($showers->count())
+                <div class="panel-footer">
+                    <div class="row">
+                        @include('home.shower')
+                    </div>
+                </div>
+            @else
+                <h3>Add Kot devices</h3>
+            @endif
+            @if(count($applies))
+                @foreach($applies as $key => $apply)
+                    {{ $apply['name'] }}
+                    <a href="{{ URL::route('acceptApply', ['userApply_id'=>$apply['user_id'],'Apply_id'=>$apply['apply_id']]) }}">
+                        <span class="glyphicon glyphicon-ok"></span>
+                    </a>
+                    <a href="{{ URL::route('removeApply', ['Apply_id'=>$apply['apply_id']]) }}">
+                        <span class="glyphicon glyphicon-remove"></span>
+                    </a>
+                @endforeach
+            @endif
+        @else
+            {{ Form::open(array('url' => URL::route('addKot'), 'method' => 'Post','class'=>'')) }}
+            <div class="form-group">
+                {{ Form::Label('kot_id', 'Kot ID')}}
+                {{ Form::text('kot_id','',['class' => 'form-control']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::Label('pass', 'In case of ownership, give password')}}
+                {{ Form::password('pass',['class' => 'form-control']) }}
+            </div>
+            {{ Form::submit('Submit!',['class'=>'btn btn-default','class'=>'btn btn-default']) }}
+            {{ Form::close() }}
+        @endif
     </div>
-    @if($showers->count())
-        @foreach($showers as $key => $shower )
-            {{ $shower->device_id }} => {{ $shower->name }}
-        @endforeach
-    @endif
-    @if(count($applies))
-        @foreach($applies as $key => $apply)
-            {{ $apply['name'] }}
-            <a href="{{ URL::route('acceptApply', ['userApply_id'=>$apply['user_id'],'Apply_id'=>$apply['apply_id']]) }}">
-                <span class="glyphicon glyphicon-ok"></span>
-            </a>
-            <a href="{{ URL::route('removeApply', ['Apply_id'=>$apply['apply_id']]) }}">
-                <span class="glyphicon glyphicon-remove"></span>
-            </a>
-        @endforeach
-    @endif
-@else
-    {{ Form::open(array('url' => URL::route('addKot'), 'method' => 'Post','class'=>'')) }}
-    <div class="form-group">
-        {{ Form::Label('kot_id', 'Kot ID')}}
-        {{ Form::text('kot_id','',['class' => 'form-control']) }}
-    </div>
-    <div class="form-group">
-        {{ Form::Label('pass', 'In case of ownership, give password')}}
-        {{ Form::password('pass',['class' => 'form-control']) }}
-    </div>
-    {{ Form::submit('Submit!',['class'=>'btn btn-default','class'=>'btn btn-default']) }}
-    {{ Form::close() }}
-@endif
+</div>
